5ae678fea30f5fc2e2b98361bae980ee81ca5a4a
Manny Rodrigues <endurtechnology@gmail.com>
2019-07-21 16:26:44 -0400

    Upgraded to v1.4

--- README.md
+++ README.md
@@ -3,29 +3,46 @@
 Contributors: Manny Rodrigues  
 Donations: https://endurtech.com/give-thanks/  
 Tags: wordpress,email  
-Requires at least: 5.0  
-Tested up to: 5.2.2  
-Stable tag: 1.0  
+Requires PHP: 7.0  
+Requires WP: 5.0+  
+Tested to: 5.2.2  
+Stable tag: 1.4  
+License: GPLv2 or later  
 
 ## Description
 
-> Outgoing email notifications are important, and you should be using your own brand and website email address. This increases the authenticity of your brand, product or service and increases name recognition among your visitors and subscribers. No doubt, to a subscriber, receiving an email from WordPress < wordpress@ > looks very unprofessional. It may even result in email delivery issues due to Internet Service Provider and end user email client spam filters. Introducing [WPFrom Email](https://endurtech.com/wpfrom-wordpress-plugin-to-change-the-default-from-email-and-name/), a free WordPress Plugin aimed at helping you quickly and easily update the default WordPress sender name and email address.  
+> Introducing [WPFrom Email](https://endurtech.com/wpfrom-wordpress-plugin-to-change-the-default-from-email-and-name/), a WordPress Plugin aimed at helping you quickly and easily update the default WordPress sender FROM Name and Email Address.  
+
+> To increase brand authenticity and create name recognition among your visitors it is recommended update how WordPress sends out emails to your subscribers. Leaving the WordPress email sending settings in their default state may even result in email delivery issues. This is because the default wordpress@ emails usually trigger Internet Service Provider and email client spam filters.  
+
+> With this plugin, you may easily update those FROM values or completely disable ALL email sending. *Be sure to test the disable email function on your server environment to ensure proper function.*  
+
+> Did our plugin save you time and add value? [Share your appreciation](https://paypal.me/endurtechnology) while supporting future improvements.  
 
 ## Features
 
-* Convenient link added under "Settings > WPFrom Email"
-* Clean and simple replacement for default WordPress Email Notifications
+* Easily replace the default WordPress email FROM Name and Email Address.  
+* Disable ALL WordPress emails. Test on your server to confirm operation.  
+* Upon deactivation, all associated database values will be removed.  
 
 ## Installation
 
-1. Upload the plugin folder to your '/wp-content/plugins/' folder
-2. Activate the plugin through the 'Plugins' menu in WordPress
+1. Upload the plugin folder to your '/wp-content/plugins/' folder.  
+2. Activate the plugin through the 'Plugins' menu in WordPress.  
+3. Visit the plugin settings area and adjust as needed.  
 
 ## Screenshots
 
-1. WPFrom Email Settings page located under "Settings > WPFrom Email"
+1. WPFrom Email Settings page located under "Settings > WPFrom Email".  
+2. WPFrom Admin Dashboard notice of disabled emails option triggered.  
 
 ## Changelog
 
-* 1.0
-  * Initial release  
+* 1.4  
+  * +Added database cleanup function upon plugin deactivation.  
+* 1.3  
+  * +Added notice of disabled emails in admin dashboard at a glance metabox.  
+* 1.2  
+  * +Added ability to disable ALL WordPress emails.  
+* 1.0  
+  * Initial release.  
--- wpfrom-email.php
+++ wpfrom-email.php
@@ -1,19 +1,18 @@
 <?php
-/*
+/* --------------------------------------------------
 Plugin Name: WPFrom Email
 Plugin URI: https://endurtech.com/wpfrom-wordpress-plugin-change-default-from-email-and-name/
-Description: Easily change the default 'from' wordpress@ email address and name.
+Description: Replaces the default WordPress email FROM Name and Email: WordPress &lt;wordpress@yourdomain.com&gt;
 Author: Manny Rodrigues
 Author URI: https://endurtech.com
-Donate link: https://endurtech.com/give-thanks/
-Version: 1.0
-Requires at least: 5.0
-Tested up to: 5.2.2
 Requires PHP: 7.0
+Requires WP: 5.0+
+Tested to: 5.2.2
+Version: 1.4
+License: GPLv2 or later
 Domain Path: languages
 Text Domain: wpfrom-emails
-License: GPLv2 or later
-*/
+-------------------------------------------------- */
 
 if ( ! defined( 'ABSPATH' ) ) {
   exit(); // Exit if accessed directly.
@@ -33,29 +32,76 @@ function wpfrom_mail_load_textdomain()
 }
 */
 
+// Add link to plugin settings from "Settings" menu
+add_action( 'admin_menu', 'wpfrom_mail_sender_menu' );
+function wpfrom_mail_sender_menu()
+{
+	add_options_page( __(WPF_SETTINGS), __(WPF_TITLE), 'manage_options', 'wpfrom-email', 'wpfrom_mail_sender_output' );
+}
+
+// Add link to plugin settings from "Plugins" page
+add_filter( 'plugin_action_links_'.plugin_basename(__FILE__), 'wpfrom_email_settings_link' );
+function wpfrom_email_settings_link( $links )
+{
+	$links[] = '<a href="' . admin_url( 'options-general.php?page=wpfrom-email' ) . '">' . __('Settings') . '</a>';
+	return $links;
+}
+
+// Add notice to admin dashboard of disabled emails
+add_filter( 'dashboard_glance_items', 'dashboardStatus' );
+function dashboardStatus( $glances )
+{
+  $email_killer_init = get_option( 'wpfrom_kill_email_id' );
+  if( $email_killer_init == '1' ) {
+    $glances[] = '<li style="float:none;"><i class="dashicons dashicons-email" aria-hidden="true" style="color:#e14d43;"></i> WordPress Emails <strong>Disabled</strong></li>';
+	}
+	return $glances;
+}
+
+// WPFrom Plugin Deactivation Database Cleanup, You're welcome!
+register_deactivation_hook( __FILE__, 'wpfrom_deactivation_cleaner' );
+function wpfrom_deactivation_cleaner()
+{
+  delete_option( 'wpfrom_custom_sender_id' );
+  delete_option( 'wpfrom_mail_sender_email_id' );
+  delete_option( 'wpfrom_mail_sender_id' ); // since 1.0, now junk
+  delete_option( 'wpfrom_mail_sender_name_id' );
+  delete_option( 'wpfrom_kill_email_id' );
+}
+
 // WordPress plugin registration
 add_action( 'admin_init', 'wpfrom_mail_sender_register' );
 function wpfrom_mail_sender_register()
 {
-	add_settings_section( 'wpfrom_mail_sender_section', WPF_SETTINGS, 'wpfrom_mail_sender_text', 'wpfrom_mail_sender' );
-
-	add_settings_field( 'wpfrom_mail_sender_email_id', __('WordPress Sender\'s Email', 'wpfrom-mail'), 'wpfrom_mail_sender_email', 'wpfrom_mail_sender', 'wpfrom_mail_sender_section' );
-
-	add_settings_field( 'wpfrom_mail_sender_id', __('WordPress Sender\'s Name', 'wpfrom-mail'), 'wpfrom_mail_sender_function', 'wpfrom_mail_sender', 'wpfrom_mail_sender_section' );
-	register_setting( 'wpfrom_mail_sender_section', 'wpfrom_mail_sender_id' );
-
-	register_setting( 'wpfrom_mail_sender_section', 'wpfrom_mail_sender_email_id' );
+  // Settings Section
+  add_settings_section( 'wpfrom_mail_sender_section', WPF_SETTINGS, 'wpfrom_mail_sender_text', 'wpfrom_mail_sender' );
+  // Enable WordPress Custom Emails
+  add_settings_field( 'wpfrom_custom_sender_id', __('WPFrom Custom Sender', 'wpfrom-mail'), 'wpfrom_custom_sender', 'wpfrom_mail_sender', 'wpfrom_mail_sender_section' );
+  register_setting( 'wpfrom_mail_sender_section', 'wpfrom_custom_sender_id' );
+  // Custom WordPress Sender Email
+	add_settings_field( 'wpfrom_mail_sender_email_id', __('Custom Senders Email', 'wpfrom-mail'), 'wpfrom_mail_sender_email', 'wpfrom_mail_sender', 'wpfrom_mail_sender_section' );
+  register_setting( 'wpfrom_mail_sender_section', 'wpfrom_mail_sender_email_id' );
+  // Custom WordPress Sender Name
+	add_settings_field( 'wpfrom_mail_sender_name_id', __('Custom Senders Name', 'wpfrom-mail'), 'wpfrom_mail_sender_name', 'wpfrom_mail_sender', 'wpfrom_mail_sender_section' );
+  register_setting( 'wpfrom_mail_sender_section', 'wpfrom_mail_sender_name_id' );
+  // Disable WordPress Emails ALL
+  add_settings_field( 'wpfrom_kill_email_id', __('WordPress Emails', 'wpfrom-mail'), 'wpfrom_mail_killer', 'wpfrom_mail_sender', 'wpfrom_mail_sender_section' );
+  register_setting( 'wpfrom_mail_sender_section', 'wpfrom_kill_email_id' );
 }
 
-// Page description
-function wpfrom_mail_sender_text()
+// Enable WPFrom Custom Sender checkbox
+function wpfrom_custom_sender()
 {
-  echo '<p>This plugin will update the default WordPress sender name and email address, <strong>WordPress < wordpress@ ></strong></p>
-  <p>To ensure delivery, use an email address that exists and associated with your present website domain.</p>
-  <p>Clear these fields and resave this form to return WordPress to its default email sending settings.</p>';
+  $custom_email = get_option( 'wpfrom_custom_sender_id' );
+  if( ! isset ( $custom_email ) || $custom_email == '' ) {
+    delete_option( 'wpfrom_custom_sender_id' );
+  } else {
+    $custom_email = 'checked="checked" ';
+  }
+  echo '<input name="wpfrom_custom_sender_id" id="wpfrom_custom_sender_id" type="checkbox" value="1" '.$custom_email.'/> <label for="wpfrom_custom_sender_id">Enable Custom WordPress Emails?</em></label>';
 }
 
-// Email field
+// FROM Email field
 function wpfrom_mail_sender_email()
 {
   $sender_email = get_option( 'wpfrom_mail_sender_email_id' );
@@ -65,14 +111,35 @@ function wpfrom_mail_sender_email()
   echo '<input name="wpfrom_mail_sender_email_id" id="wpfrom_mail_sender_email_id" type="email" placeholder="wordpress@mywebsite.com" class="regular-text" value="' . $sender_email . '" />';
 }
 
-// From field
-function wpfrom_mail_sender_function()
+// FROM Name field
+function wpfrom_mail_sender_name()
 {
-  $sender_name = get_option( 'wpfrom_mail_sender_id' );
+  $sender_name = get_option( 'wpfrom_mail_sender_name_id' );
   if( $sender_name == '' ) {
-    delete_option( 'wpfrom_mail_sender_id' );
+    delete_option( 'wpfrom_mail_sender_name_id' );
   }
-  echo '<input name="wpfrom_mail_sender_id" id="wpfrom_mail_sender_id" type="text" placeholder="WordPress" class="regular-text" value="' . $sender_name . '" />';
+  echo '<input name="wpfrom_mail_sender_name_id" id="wpfrom_mail_sender_name_id" type="text" placeholder="WordPress" class="regular-text" value="' . $sender_name . '" />';
+}
+
+// Disable WordPress Emails checkbox
+function wpfrom_mail_killer()
+{
+  $email_killer = get_option( 'wpfrom_kill_email_id' );
+  if( ! isset ( $email_killer ) || $email_killer == '' ) {
+    delete_option( 'wpfrom_kill_email_id' );
+  } else {
+    $email_killer = 'checked="checked" ';
+  }
+  echo '<input name="wpfrom_kill_email_id" id="wpfrom_kill_email_id" type="checkbox" value="1" '.$email_killer.'/> <label for="wpfrom_kill_email_id">Disable ALL WordPress Emails? Kills wp_mail(), <em>slowly.</em></label>';
+}
+
+// Page description
+function wpfrom_mail_sender_text()
+{
+  echo '<p>Replaces the default WordPress email FROM <strong>Name</strong> and <strong>Email</strong>: <strong>WordPress &lt;wordpress@yourdomain.com&gt;</strong></p>
+  <p>Enable and set your FROM Email (<em>required</em>) and Name. To aid email delivery, use a real address on your server.</p>
+  <p>Checking "Disable" option will disable ALL emails. <strong>WARNING!</strong> Fully test it on your website to confirm operation.</p>
+  <p>Did <a href="https://endurtech.com/wpfrom-wordpress-plugin-to-change-the-default-from-email-and-name/" target="_blank" title="Opens New Window">our plugin</a> save you time and add value? <a href="https://paypal.me/endurtechnology" target="_blank" title="Opens New Window"><strong>Share your appreciation</strong></a> while supporting future improvements.</p>';
 }
 
 // Settings form
@@ -85,37 +152,42 @@ function wpfrom_mail_sender_output()
       submit_button();
   echo '</form>
   </div>';
-  // Donate
-  echo '<div style="padding:50px;">
-  <p>Did this Plugin Add Value to You? <a href="https://paypal.me/endurtechnology" target="_blank" title="Opens New Window"><strong>Share Your Appreciation</strong></a>!</p>
-  </div>';
 }
 
-// Add plugin settings page link under Settings menu
-add_action( 'admin_menu', 'wpfrom_mail_sender_menu' );
-function wpfrom_mail_sender_menu()
+// Replace default WordPress emails
+add_filter( 'wp_mail', 'wpfrom_custom_sender_init' );
+function wpfrom_custom_sender_init()
 {
-	add_options_page( __(WPF_SETTINGS), __(WPF_TITLE), 'manage_options', 'wpfrom-email', 'wpfrom_mail_sender_output' );
+  $custom_sender_init = get_option( 'wpfrom_custom_sender_id' );
+  if( $custom_sender_init == '1' ) {
+    add_filter( 'wp_mail_from', 'wpfrom_new_mail_from' );
+    add_filter( 'wp_mail_from_name', 'wpfrom_new_mail_from_name' );
+  }
 }
 
-// Add link to plugin settings from Plugins page
-add_filter( 'plugin_action_links_'.plugin_basename(__FILE__), 'wpfrom_email_settings_link' );
-function wpfrom_email_settings_link( $links )
-{
-	$links[] = '<a href="' . admin_url( 'options-general.php?page=wpfrom-email' ) . '">' . __('Settings') . '</a>';
-	return $links;
-}
-
-// Replace the default from wordpress@ email address
-add_filter( 'wp_mail_from', 'wpfrom_new_mail_from' );
-function wpfrom_new_mail_from( $old )
+// Replace the default FROM Email: wordpress@yourdomain.com
+function wpfrom_new_mail_from()
 {
   return get_option( 'wpfrom_mail_sender_email_id' );  
 }
 
-// Replace the default from wordpress@ name
-add_filter( 'wp_mail_from_name', 'wpfrom_new_mail_from_name' );
-function wpfrom_new_mail_from_name( $old )
+// Replace the default FROM Name: WordPress
+function wpfrom_new_mail_from_name()
 {
-  return get_option( 'wpfrom_mail_sender_id' );
+  return get_option( 'wpfrom_mail_sender_name_id' );
+}
+
+// Disable all WordPress email. Hook into phpmailer class, ClearAllRecipients.
+// Thanks: https://wordpress.stackexchange.com/users/111056/kirillrocks
+add_filter( 'wp_mail', 'wpfrom_email_killer' );
+function wpfrom_email_killer( $args )
+{
+  $email_killer_init = get_option( 'wpfrom_kill_email_id' );
+  if( $email_killer_init == '1' ) {
+    add_action( 'phpmailer_init', 'kickout_recipients' );
+  }
+}
+function kickout_recipients( $phpmailer )
+{
+  $phpmailer->ClearAllRecipients();
 }
