0e2b7829dd1a1fbb7f6aa39f2c0c726f3379d822
Manny Rodrigues <endurtechnology@gmail.com>
2019-12-10 06:41:30 -0500

    updated version from 1.7 to 1.7.0

--- wpfrom-email.php
+++ wpfrom-email.php
@@ -9,7 +9,7 @@ License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Requires at least: 5.0
 Tested up to: 5.3
-Version: 1.7
+Version: 1.7.0
 Text Domain: wpfrom-emails
 Domain Path: /locale
 
